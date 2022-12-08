9d3e3719b66021d8a05497dd737b0e5f2e9aefa7
KoneMango <98529557+KoneMango@users.noreply.github.com>
2022-12-08 21:57:46 +1100

    videoCapture

--- Two_sum.py
+++ Two_sum.py
@@ -1,2 +1,22 @@
 import numpy as nps
+import cv2
+#从文件中读取一张图片
+def read_image(path):
+    img = cv2.imread(path)
+    return img  
+#主函数，弹出框让用户选择一张图片
+if __name__=="__main__":
+    print('Please select an image')
+    path = "C:/Users/will/Desktop/gront.jpg"
+    img = read_image(path)
+    resized_image = cv2.resize(img, (600, 400))
+    cv2.imshow('image',resized_image)
+    cv2.resizeWindow('image', 600, 400)
+    cv2.waitKey(50000)
+
+
+
+
+
+
 
